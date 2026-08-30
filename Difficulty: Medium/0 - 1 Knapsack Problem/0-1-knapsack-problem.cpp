e8ea4282a9b70f51bae84125fccdbd419999e3f7
vedantmagarde <vedantmagarde@gmail.com>
2026-08-30 15:50:35 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
@@ -1,20 +1,21 @@
 class Solution {
 	public:
 	
-	int fun(vector<int>&wt, int W, vector<int>&val, int i, vector<vector<int>> &dp) {
-		if (i >= wt.size())return 0;
-		if (dp[W][i] != -1)
-			return dp[W][i];
-		int skip = fun(wt, W, val, i + 1, dp);
+	int fun(int W, vector<int> &val, vector<int> &wt, int i, vector<vector<int>> &dp) {
+		if (i >= wt.size())
+			return 0;
 		
+		if (dp[W][i] != -1)return dp[W][i];
+		
+		int skip = fun (W, val, wt, i + 1, dp);
 		int take = 0;
 		if (W - wt[i] >= 0)
-			take = val[i]+ fun(wt, W - wt[i], val, i + 1, dp);
+			take += val[i]+ fun(W - wt[i], val, wt, i + 1, dp);
 		
 		return dp[W][i] = max(skip, take);
 	}
 	int knapsack(int W, vector<int> &val, vector<int> &wt) {
-		vector<vector<int>> dp(W + 1, vector<int>(val.size() + 1, -1));
-		return fun(wt, W, val, 0, dp);
+		vector<vector<int>> dp(W + 1, vector<int>(wt.size() + 1, -1));
+		return fun(W, val, wt, 0, dp);
 	}
 };
