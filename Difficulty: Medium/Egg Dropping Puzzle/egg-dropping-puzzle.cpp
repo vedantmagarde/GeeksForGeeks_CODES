1f790f8f7eee2ffa0d50a3010fd5e2ec0cd3ecec
vedantmagarde <vedantmagarde@gmail.com>
2026-09-16 15:50:48 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Egg Dropping Puzzle/egg-dropping-puzzle.cpp
+++ Difficulty: Medium/Egg Dropping Puzzle/egg-dropping-puzzle.cpp
@@ -1,38 +1,35 @@
 class Solution {
 	public:
 	int fun(int n, int k, vector<vector<int>> &dp) {
-		
-		if (k <= 0 || n <= 0)
-			return 0;
-		
-		if (k == 1)
-			return 1;
-		if (n == 1)
-			return k;
+		if (k <= 0 || n <= 0)return 0;
+		if (k == 1)return 1;
+		if (n == 1)return k;
 		
 		if (dp[n][k] != -1)return dp[n][k];
-		int min_moves = INT_MAX;
-		int low = 1, high = k;
 		
-		while (low <= high) {
-			int mid = low + (high - low) / 2;
+		int l = 1;
+		int h = k;
+		int ans = INT_MAX;
+		while (l <= h) {
+			int mid = l + (h - l)/2;
 			
-			int breaki = fun(n - 1, mid - 1, dp);
-			int not_ = fun(n, k - mid, dp);
+			int broken = fun(n - 1, mid - 1, dp);
+			int survived = fun(n, k - mid, dp);
 			
-			int worst_case = 1 + max(breaki, not_);
-			min_moves = min(min_moves, worst_case);
+			int chances = 1 + max(broken, survived);
+			ans = min(ans, chances);
 			
-			if (breaki < not_) {
-				low = mid + 1;
+			if (broken<survived) {
+				l = mid + 1;
 			} else {
-				high = mid - 1;
+				h = mid - 1;
 			}
+			
 		}
-		return dp[n][k] = min_moves;
+		return dp[n][k] = ans;
 	}
 	int eggDrop(int n, int k) {
-		vector<vector<int>> dp(n + 1, vector<int>(k + 1, -1));
-		return fun(n, k, dp);
+		vector<vector<int>> dp(n + 2, vector<int>(k + 2, -1));
+		return fun(n, k,dp);
 	}
 };
